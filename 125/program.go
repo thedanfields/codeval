352163255ec1d718de0f9ff5587d8f59248d0003
Dan <daniel.laurent.fields@gmail.com>
2014-08-11 22:32:43 -0400

    Added solution for 125

---
+++ 125/program.go
@@ -0,0 +1,58 @@
+
+package main
+
+import "fmt"
+import "log"
+import "bufio"
+import "os"
+import "strconv"
+
+func main() {
+    file, err := os.Open(os.Args[1])
+    if err != nil {
+        log.Fatal(err)
+    }   
+    defer file.Close()
+    scanner := bufio.NewScanner(file)
+    for scanner.Scan() {
+        //'scanner.Text()' represents the test case, do something with it
+        atPosition, _ := strconv.Atoi(scanner.Text())
+        numberOfHalves := 0
+	    for i := atPosition; i >= 1; i = fuzzyHalfer(i) {
+		    numberOfHalves++
+	    }
+	    
+	     fmt.Println(determineNumber(numberOfHalves))
+       
+    }   
+}
+
+
+
+func fuzzyHalfer(someIndex int) (nextIndex int) {
+
+	for nextIndex = someIndex; nextIndex != 0 && (nextIndex%2 == 0 || nextIndex == someIndex); {
+		nextIndex /= 2
+	}
+
+	// if we initially started on an even number we'll need to take one more half
+	if someIndex%2 == 0 {
+		nextIndex /= 2
+	}
+
+	return nextIndex
+}
+
+func determineNumber(numberOfHalves int) (theNumber int) {
+
+	switch numberOfHalves % 3 {
+	case 0:
+		theNumber = 0
+	case 2:
+		theNumber = 2
+	case 1:
+		theNumber = 1
+	}
+
+	return
+}
